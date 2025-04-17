f49387547223428030e89d4d3d1b0775e3b71764
marc treu <mr.treu@gmail.com>
2025-04-17 23:03:09 +0200

    change how the enemy are generated

--- path_spawner.gd
+++ path_spawner.gd
@@ -1,7 +1,23 @@
-extends Node2D
+extends Node
 
-@onready var path = preload("res://stage1.tscn")
+@onready var path = get_parent().get_node("Path2D")
+# @onready var path = get_parent().get_node("Player")
+@onready var EnemyScene = preload("res://dog.tscn")
+@onready var timer = $Timer
+
+func _ready():
+	timer.start()  # Start the timer to auto-spawn enemies
+
+func spawn_enemy():
+	var enemy = EnemyScene.instantiate()
+	var follow = PathFollow2D.new()
+	path.add_child(follow)
+	follow.add_child(enemy)
+	enemy.scale = Vector2(-2.0, -2.0)
+	
+	# Connect the enemy_killed signal if it exists
+	# if enemy.has_signal("enemy_killed"):
+	# 	enemy.enemy_killed.connect(player.add_money)
 
 func _on_timer_timeout() -> void:
-	var tmpPath = path.instantiate()
-	add_child(tmpPath)
+	spawn_enemy()
