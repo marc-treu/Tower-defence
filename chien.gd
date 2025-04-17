b7c5c22dcca6e697213857a38f8bb618ff9c6994
marc treu <mr.treu@gmail.com>
2025-04-17 20:19:30 +0200

    add attack mecanique and dog spawn with differente speed

--- chien.gd
+++ chien.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
 @export var speed = 0.1
+@export var ratio = 0
+var heath = 100
 
-func _process(delta):
-	var ratio = get_parent().get_progress_ratio()
+func _ready() -> void:
+	self.speed = 0.1 + randi_range(1, 10) / 200.0
+
+func take_damage(damage):
+	self.heath -= damage
+	if self.heath <= 0:
+		self.queue_free()
+
+
+func _physics_process(delta):
+	ratio = get_parent().get_progress_ratio()
 	ratio += delta * speed
 	# ratio = ratio + delta * speed
 	get_parent().set_progress_ratio(ratio)
--- tower.gd
+++ tower.gd
@@ -2,15 +2,33 @@ extends CharacterBody2D
 
 
 var targets = []
+var cur_target = null
+var damage: int = 10
+var cool_down: int = 10
+var cur_cool_down: int = 0
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	targets = get_node("col").get_overlapping_bodies()
-	if targets:
+	if body != self:
+		targets.append(body)
 		
-		for target in targets:
-			if "chien" in target.name:
-				target.queue_free()
+
+func get_target():
+	var max_val = 0
+	for target in targets:
+		if target.ratio > max_val:
+			max_val = target.ratio
+			cur_target = target
+	
+
+func _physics_process(delta):
+	if cur_cool_down == 0:
+		get_target()
+		if cur_target:
+			cur_target.take_damage(damage)
+			cur_cool_down = 10
+	else:
+		cur_cool_down -= 1
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	targets.erase(body)
